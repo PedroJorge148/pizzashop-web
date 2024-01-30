709221c545cc2b6236787f19ce5cde93ea7442c1
Pedro Jorge <pedrojorge2011@gmail.com>
2024-01-30 10:32:15 -0300

    test setup

---
+++ src/components/order-status.spec.ts
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react'
+
+import { OrderStatus } from './order-status'
+
+describe('Order Status', () => {
+  it('should display the right text based on order status', () => {
+    const wrapper = render(<OrderStatus status="pending" />)
+
+    wrapper.debug()
+
+  })
+})
